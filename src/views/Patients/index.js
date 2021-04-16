033a3f8dcaafdb2d6d52e79a4ff72f6863fa76a9
sanyath007 <sanyath007@gmail.com>
2021-04-16 12:59:45 +0700

    Re-design Patients view component and use api to get data from db

--- src/views/Patients/index.js
+++ src/views/Patients/index.js
@@ -1,58 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
-
-const patientData = [
-  {
-    code: '10884A080421CD0002',
-    name: 'นายชัชวาลย์ คงสันเทียะ',
-    hn: '33394',
-    sex: '1',
-    age_y: 19,
-    bed: '',
-    reg_date: '2021-04-09',
-    lab_date: '2021-04-08',
-    lab_result: 'Positive',
-    dx: 'Covid19',
-    symptom: 'ไม่มีไข้ ไม่ไอ ไม่มีอาการหอบเหนื่อย O2 sat RA=97-98%',
-    dch_date: '',
-    adm_date: 0,
-    remark: ''
-  },
-  {
-    code: '10884A090421PJ0015',
-    name: 'นายปฏิหารย์ จันทร์พลงาม',
-    hn: '96202',
-    sex: '1',
-    age_y: 18,
-    bed: '',
-    reg_date: '2021-04-10',
-    lab_date: '2021-04-09',
-    lab_result: 'Positive',
-    dx: 'Covid19',
-    symptom: 'ไม่มีน้ำมูล ไม่ไอ ไม่ไข้ ไม่มีหอบ',
-    dch_date: '',
-    adm_date: 0,
-    remark: ''
-  },
-  {
-    code: '10884A010421TS0010',
-    name: 'นายธนโชติ สุขจิตร์',
-    hn: '104950',
-    sex: '1',
-    age_y: 19,
-    bed: '',
-    reg_date: '2021-04-11',
-    lab_date: '2021-04-10',
-    lab_result: 'Positive',
-    dx: 'Covid19',
-    symptom: 'ไม่ไข้ ไม่ไอ ไม่หอบ',
-    dch_date: '',
-    adm_date: 0,
-    remark: ''
-  }
-];
+import api from '../../api';
 
 const Patients = () => {
+  const [patients, setPatients] = useState([]);
+
+  const fetchPatients = async () => {
+    let res = await api.get(`/patients`);
+
+    setPatients(res.data.items);
+  };
+
+  useEffect(() => {
+    fetchPatients();
+  }, []);
 
   return (
     <div className="container-fluid">
@@ -61,48 +22,65 @@ const Patients = () => {
 
           <div className="card">
             <div className="card-header">
-              <h3 className="card-title">Striped Full Width Table</h3>
+              <div className="row">
+                <div className="col-md-6">
+                  <h3 className="card-title">Striped Full Width Table</h3>
+                </div>
+                <div className="col-md-6">
+                  <Link to="/patients/add" className="btn btn-md bg-primary float-right">เพิ่มผู้ป่วย</Link>
+                </div>
+              </div>
             </div>
             <div className="card-body p-0">
               <table className="table table-striped">
                 <thead>
-                  <tr>
-                    <th style={{ width: '10px' }}>#</th>
-                    <th>Code</th>
-                    <th>HN</th>
+                  <tr style={{ fontSize: '14px' }}>
+                    <th style={{ width: '4%', textAlign: 'center' }}>#</th>
+                    <th style={{ width: '10%', textAlign: 'center' }}>Code</th>
+                    <th style={{ width: '8%', textAlign: 'center' }}>HN</th>
                     <th>ชื่อ-สกุล</th>
-                    <th>เพศ</th>
-                    <th>อายุ (ปี)</th>
-                    <th>วันที่ Admit</th>
-                    <th>วันที่ส่ง Lab</th>
-                    <th>ผล Lab</th>
-                    <th>Dx</th>
-                    <th>อาการโดยรวม</th>
-                    <th>วันที่ D/C</th>
-                    <th>จำนวนวันนอน รพ.</th>
-                    <th>หมายเหตุ</th>
-                    <th style={{ width: '40px' }}>Actions</th>
+                    <th style={{ width: '5%', textAlign: 'center' }}>เพศ</th>
+                    <th style={{ width: '5%', textAlign: 'center' }}>อายุ (ปี)</th>
+                    <th style={{ width: '8%', textAlign: 'center' }}>วันที่ Admit</th>
+                    <th style={{ width: '8%', textAlign: 'center' }}>วันที่ส่ง Lab</th>
+                    <th style={{ width: '6%', textAlign: 'center' }}>ผล Lab</th>
+                    <th style={{ width: '6%', textAlign: 'center' }}>Dx</th>
+                    {/* <th>อาการโดยรวม</th> */}
+                    <th style={{ width: '8%', textAlign: 'center' }}>วันที่ D/C</th>
+                    <th style={{ width: '6%', textAlign: 'center' }}>จน.วันนอน</th>
+                    {/* <th>หมายเหตุ</th> */}
+                    <th style={{ width: '10%', textAlign: 'center' }}>Actions</th>
                   </tr>
                 </thead>
                 <tbody>
-                  {patientData.map((patient, index) => {
+                  {patients.map((patient, index) => {
                     return (
-                      <tr>
-                        <td>{index + 1}</td>
-                        <td>{patient.code}</td>
-                        <td>{patient.hn}</td>
-                        <td>{patient.name}</td>
-                        <td>{patient.sex ? 'ชาย' : 'หญิง'}</td>
-                        <td>{patient.age_y}</td>
-                        <td>{patient.reg_date}</td>
-                        <td>{patient.lab_date}</td>
-                        <td>{patient.lab_result}</td>
-                        <td>{patient.dx}</td>
-                        <td>{patient.symptom}</td>
-                        <td>{patient.dch_date}</td>
-                        <td>{patient.adm_date}</td>
-                        <td>{patient.remark}</td>
-                        <td><span className="badge bg-danger">55%</span></td>
+                      <tr key={patient.hn}>
+                        <td style={{ textAlign: 'center' }}>{index + 1}</td>
+                        <td style={{ textAlign: 'center' }}>{patient.code}</td>
+                        <td style={{ textAlign: 'center' }}>{patient.hn}</td>
+                        <td style={{ fontSize: '14px' }}>{patient.name}</td>
+                        <td style={{ textAlign: 'center' }}>{patient.sex ? 'ชาย' : 'หญิง'}</td>
+                        <td style={{ textAlign: 'center' }}>{patient.age_y}</td>
+                        <td style={{ textAlign: 'center' }}>{patient.reg_date}</td>
+                        <td style={{ textAlign: 'center' }}>{patient.lab_date}</td>
+                        <td style={{ textAlign: 'center' }}>{patient.lab_result}</td>
+                        <td style={{ textAlign: 'center' }}>{patient.dx}</td>
+                        {/* <td style={{ fontSize: '14px' }}>{patient.symptom}</td> */}
+                        <td style={{ textAlign: 'center' }}>{patient.dch_date}</td>
+                        <td style={{ textAlign: 'center' }}>{patient.adm_date}</td>
+                        {/* <td>{patient.remark}</td> */}
+                        <td style={{ textAlign: 'center' }}>
+                          <Link className="btn btn-sm bg-info">
+                            <i class="fas fa-search"></i>
+                          </Link>
+                          <Link className="btn btn-sm bg-warning">
+                            <i class="fas fa-edit"></i>
+                          </Link>
+                          <Link className="btn btn-sm bg-danger">
+                            <i class="fas fa-trash-alt"></i>
+                          </Link>
+                        </td>
                       </tr>
                     );
                   })}
@@ -110,7 +88,7 @@ const Patients = () => {
               </table>
             </div>
             <div className="card-footer clearfix">
-              <ul className="pagination pagination-sm m-0 float-right">
+              <ul className="pagination pagination-md m-0 float-right">
                 <li className="page-item"><a className="page-link" href="#">&laquo;</a></li>
                 <li className="page-item"><a className="page-link" href="#">1</a></li>
                 <li className="page-item"><a className="page-link" href="#">2</a></li>
