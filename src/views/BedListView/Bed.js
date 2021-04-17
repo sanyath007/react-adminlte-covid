7e0668d026b395660245d2ad949723fcc469030e
sanyath007 <sanyath007@gmail.com>
2021-04-17 19:00:55 +0700

    Edit Beds/Bed by adding onDischarge and onAdmit function

--- src/views/BedListView/Bed.js
+++ src/views/BedListView/Bed.js
@@ -20,6 +20,16 @@ const Bed = ({ bed }) => {
     setStatus(regStates.find(state => state.id.toString() === res.data.used?.reg_state));
   };
 
+  const onDischarge = async (e) => {
+    e.preventDefault();
+    // TODO: to discharge patient from bed when D/C btn is clicked
+  };
+
+  const onAdmit = async (e) => {
+    e.preventDefault();
+    // TODO: to admit patient without register from bed when admit btn is clicked
+  };
+
   useEffect(() => {
     fetchBedUsed();
   }, [bed]);
@@ -44,14 +54,22 @@ const Bed = ({ bed }) => {
         </span>
         <div style={{ flex: '1', justifyContent: 'space-between', marginTop: '5px' }}>
           {bed?.bed_status === '0' && (
-            <a href="#" className="btn btn-sm text-primary float-right">
+            <a
+              href="#"
+              className="btn btn-sm text-primary float-right"
+              onClick={(e) => onAdmit(e)}
+            >
               <i class="fas fa-hospital-user"></i>
               Admit
             </a>
           )}
           
           {bed?.bed_status === '1' && (
-            <a href="#" className="btn btn-sm text-danger float-right">
+            <a
+              href="#"
+              className="btn btn-sm text-danger float-right"
+              onClick={(e) => onDischarge(e)}
+            >
               D/C
               <i class="fas fa-sign-out-alt ml-1"></i>
             </a>
