2ae1628e68dbd993c5ce6b0da5748cd0140be2f8
sanyath007 <sanyath007@gmail.com>
2021-04-20 09:41:26 +0700

    Modify Signin component by use Formik, Yup and Form of react-bootstrap

--- src/views/Signin/index.js
+++ src/views/Signin/index.js
@@ -1,69 +1,121 @@
-import React from 'react'
+import React from 'react';
+import { Form, Formik, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { Form as BsForm } from 'react-bootstrap';
 
 const Signin = () => {
+  const authSchema = Yup.object().shape({
+    username: Yup.string().required('Username is required!!'),
+    password: Yup.string().required('Password is required!!'),
+  });
+
+  const onSubmit = async (values, props) => {
+    const { username, password } = values;
+    console.log(username, password);
+
+    // props.resetForm();
+  };
+
   return (
-    <div className="login-box">
-      <div className="login-logo">
-        <a href="../../index2.html"><b>Admin</b>LTE</a>
-      </div>
+    <Formik
+      initialValues={{
+        username: '',
+        password: ''
+      }}
+      validationSchema={authSchema}
+      onSubmit={onSubmit}
+    >
+      {(formik) => {
+        return (
+          <div className="login-box">
+            <div className="login-logo">
+              <a href="../../index2.html"><b>Admin</b>LTE</a>
+            </div>
 
-      {/* /.login-logo */}
-      <div className="card">
-        <div className="card-body login-card-body">
-          <p className="login-box-msg">Sign in to start your session</p>
+            {/* /.login-logo */}
+            <div className="card">
+              <div className="card-body login-card-body">
+                <p className="login-box-msg">Sign in to start your session</p>
 
-          <form action="../../index3.html" method="post">
-            <div className="input-group mb-3">
-              <input type="email" className="form-control" placeholder="Email" />
-              <div className="input-group-append">
-                <div className="input-group-text">
-                  <span className="fas fa-envelope"></span>
-                </div>
+                <Form>
+                  <div className="input-group mb-3">
+                    <BsForm.Control
+                      type="text"
+                      name="username"
+                      value={formik.values.username}
+                      onChange={formik.handleChange}
+                      className="form-control"
+                      placeholder="Username"
+                      isInvalid={formik.errors.username && formik.touched.username}
+                    />
+                    <div className="input-group-append">
+                      <div className="input-group-text">
+                        <span className="fas fa-user"></span>
+                      </div>
+                    </div>
+                    <ErrorMessage
+                      name="username"
+                      render={msg => <span className="invalid-feedback">{msg}</span>}
+                    />
+                  </div>
+                  <div className="input-group mb-3">
+                    <BsForm.Control
+                      type="password"
+                      name="password"
+                      value={formik.values.password}
+                      onChange={formik.handleChange}
+                      className="form-control"
+                      placeholder="Password"
+                      isInvalid={formik.errors.password && formik.touched.password}
+                    />
+                    <div className="input-group-append">
+                      <div className="input-group-text">
+                        <span className="fas fa-lock"></span>
+                      </div>
+                    </div>
+                    <ErrorMessage
+                      name="password"
+                      render={msg => <span className="invalid-feedback">{msg}</span>}
+                    />
+                  </div>
+                  <div className="row">
+                    <div className="col-8">
+                      <div className="icheck-primary">
+                        <input type="checkbox" id="remember" />
+                        <label htmlFor="remember">
+                          Remember Me
+                        </label>
+                      </div>
+                    </div>
+                    <div className="col-4">
+                      <button type="submit" className="btn btn-primary btn-block">Sign In</button>
+                    </div>
+                  </div>
+                </Form>
+
+                {/* /.social-auth-links */}
+                {/* <div className="social-auth-links text-center mb-3">
+                  <p>- OR -</p>
+                  <a href="#" className="btn btn-block btn-primary">
+                    <i className="fab fa-facebook mr-2"></i> Sign in using Facebook
+                  </a>
+                  <a href="#" className="btn btn-block btn-danger">
+                    <i className="fab fa-google-plus mr-2"></i> Sign in using Google+
+                  </a>
+                </div> */}
+
+                <p className="mb-1">
+                  <a href="forgot-password.html">I forgot my password</a>
+                </p>
+                <p className="mb-0">
+                  <a href="register.html" className="text-center">Register a new membership</a>
+                </p>
               </div>
             </div>
-            <div className="input-group mb-3">
-              <input type="password" className="form-control" placeholder="Password" />
-              <div className="input-group-append">
-                <div className="input-group-text">
-                  <span className="fas fa-lock"></span>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-8">
-                <div className="icheck-primary">
-                  <input type="checkbox" id="remember" />
-                  <label htmlFor="remember">
-                    Remember Me
-                  </label>
-                </div>
-              </div>
-              <div className="col-4">
-                <button type="submit" className="btn btn-primary btn-block">Sign In</button>
-              </div>
-            </div>
-          </form>
-
-          {/* /.social-auth-links */}
-          <div className="social-auth-links text-center mb-3">
-            <p>- OR -</p>
-            <a href="#" className="btn btn-block btn-primary">
-              <i className="fab fa-facebook mr-2"></i> Sign in using Facebook
-            </a>
-            <a href="#" className="btn btn-block btn-danger">
-              <i className="fab fa-google-plus mr-2"></i> Sign in using Google+
-            </a>
           </div>
-
-          <p className="mb-1">
-            <a href="forgot-password.html">I forgot my password</a>
-          </p>
-          <p className="mb-0">
-            <a href="register.html" className="text-center">Register a new membership</a>
-          </p>
-        </div>
-      </div>
-    </div>
+        );
+      }}
+    </Formik>
   );
 };
 
