4e36e532934c4a74b6000b139ff5eb8685b28a51
sanyath007 <sanyath007@gmail.com>
2021-04-21 09:21:53 +0700

    Edit QuickStatsCard of Dashboard view by adding QuickStatsCard of bed data and change data of old

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -8,24 +8,73 @@ const Dashboard = () => {
   return (
     <div className="container-fluid">
       <div className="row">
-
+        {/* ยอดผู้ป่วย */}
         <div className="col-lg-3 col-6">
           <QuickStatsCard
             bg="bg-info"
-            stat={<span>150<sup style={{ fontSize: '16px' }}>m$</sup></span>}
-            name="New Orders"
-            icon="ion-bag"
+            stat={<span>0<sup style={{ fontSize: '18px' }}>ราย</sup></span>}
+            name="ผู้ป่วยรายใหม่"
+            icon="ion-person-add"
             url="/"
           />
         </div>
         <div className="col-lg-3 col-6">
-          <QuickStatsCard bg="bg-success" stat={['53', <sup style={{ fontSize: '20px' }}>%</sup>]} name="Bounce Rate" icon="ion-stats-bars" url="/" />
+          <QuickStatsCard
+            bg="bg-success"
+            stat={['20', <sup style={{ fontSize: '18px' }}>ราย</sup>]}
+            name="ผู้ป่วยสะสม"
+            icon="ion-stats-bars"
+            url="/"
+          />
         </div>
         <div className="col-lg-3 col-6">
-          <QuickStatsCard bg="bg-warning" stat="44" name="User Registrations" icon="ion-person-add" url="/" />
+          <QuickStatsCard
+            bg="bg-warning"
+            stat={['44', <sup style={{ fontSize: '18px' }}>ราย</sup>]}
+            name="จำหน่วยแล้ว"
+            icon="ion-share"
+            url="/"
+          />
         </div>
         <div className="col-lg-3 col-6">
-          <QuickStatsCard bg="bg-danger" stat="65" name="Unique Visitors" icon="ion-pie-graph" url="/" />        
+          <QuickStatsCard
+            bg="bg-danger"
+            stat={['0', <sup style={{ fontSize: '18px' }}>ราย</sup>]}
+            name="เสียชีวิต"
+            icon="ion-pie-graph"
+            url="/"
+          />        
+        </div>
+
+        {/* เตียง */}
+        <div className="col-lg-4 col-6">
+          <QuickStatsCard
+            bg="bg-primary"
+            stat={['30', <sup style={{ fontSize: '18px' }}>ราย</sup>]}
+            name="เตียงทั้งหมด"
+            icon="ion-archive"
+            url="/"
+          />
+        </div>
+
+        <div className="col-lg-4 col-6">
+          <QuickStatsCard
+            bg="bg-danger"
+            stat={['20', <sup style={{ fontSize: '18px' }}>เตียง</sup>]}
+            name="มีผู้ป่วย"
+            icon="ion-person-stalker"
+            url="/"
+          />
+        </div>
+
+        <div className="col-lg-4 col-6">
+          <QuickStatsCard
+            bg="bg-default"
+            stat={<span>10<sup style={{ fontSize: '18px' }}>เตียง</sup></span>}
+            name="เตียงว่าง"
+            icon="ion-battery-empty"
+            url="/"
+          />        
         </div>
 
       </div>{/* /.row */}
