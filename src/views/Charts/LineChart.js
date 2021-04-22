d8726fb2d32a191b25f41492cf9f59448eb724e9
sanyath007 <sanyath007@gmail.com>
2021-04-22 21:34:12 +0700

    Create new Charts/LineChart and use it in Dashboard/LineChartCard

---
+++ src/views/Charts/LineChart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+
+const LineChart = ({ data, options }) => {
+  return (
+    <>
+      <Line data={data} options={options} />
+    </>
+  );
+};
+
+export default LineChart;
---
+++ src/views/Dashboard/PieChartCard.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import LineChart from '../Charts/LineChart';
+
+const data = {
+  labels: ['1', '2', '3', '4', '5', '6'],
+  datasets: [
+    {
+      label: '# of votes',
+      data: [12, 19, 3, 5, 2, 3],
+      fill: false,
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgba(255, 99, 132, 0.2)',
+    }
+  ]
+};
+
+const options = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+};
+
+const PieChartCard = () => {
+  return (
+    <div className="card">
+      <div className="card-header">
+        <h3 className="card-title">
+          <i className="fas fa-chart-pie mr-1"></i>
+          Pie Chart
+        </h3>
+      </div>
+      <div className="card-body">
+        <div className="chart" id="revenue-chart" style={{ position: 'relative' }}>
+
+          <LineChart data={data} options={options} />
+        
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PieChartCard;
