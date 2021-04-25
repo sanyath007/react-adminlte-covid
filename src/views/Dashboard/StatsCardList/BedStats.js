ee76f840e8d15230f9ca1aef0ce198bcc602f454
sanyath007 <sanyath007@gmail.com>
2021-04-25 12:08:12 +0700

    Move QuickStatsCard to components/QuickStatsCard

--- src/views/Dashboard/StatsCardList/BedStats.js
+++ src/views/Dashboard/StatsCardList/BedStats.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../../api';
-import QuickStatsCard from './QuickStatsCard'
+import QuickStatsCard from '../../../components/QuickStatsCard'
 
 const BedStats = () => {
   const [stats, setStats] = useState(null);
